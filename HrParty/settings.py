c1d6cd208a229c48c1931c359ed4245cfdd20686
Shivam <shivam@indwealth.in>
2020-10-12 03:06:37 +0530

    Added: gunicorn

--- HrParty/settings.py
+++ HrParty/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +127,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = "party/static"
 # JQUERY_URL = True
 USE_DJANGO_JQUERY = True
+
+django_heroku.settings(locals())
